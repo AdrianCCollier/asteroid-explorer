06a1edfce3df0ac7868155f6101bb4ce915409f7
Adrian <AdrianCCollier@gmail.com>
2023-09-05 19:01:06 -0600

    package.json updates

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = process.env.PORT || 3000;
+const API_KEY = 'umZEqICZSHCfh7IQRl6KRpgR2LiouHORozc7ZzXM';
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+app.get('/asteroids', async (req, res) => {
+  try {
+    const response = await axios.get(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`);
+    res.send(response.data);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .send('Error occured while fetching data from NASA API', error)
+  }
+});
 
-app.listen(port, () => {
-  console.log(`Successfully listening on port ${port}`);
-})
+app.listen(port, () => console.log(`Server listening on port ${port}`));
