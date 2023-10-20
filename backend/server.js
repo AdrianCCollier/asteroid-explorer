297c0f236c321f2db045bfe97076a1acf13deae2
Adrian <AdrianCCollier@gmail.com>
2023-10-20 09:36:44 -0600

    API Caching

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,12 @@ app.use(express.static(path.join(__dirname, '../frontend/build')))
 app.use(express.json())
 
 const API_KEY = process.env.API_KEY
+const CACHE_DURATION = 744 * 3600 * 1000 // Data is cached for a month
+const CACHE_FILE = path.join(__dirname, 'asteroidsCache.json')
 
 const { connectToDatabase, db } = require('./database/database')
 
-connectToDatabase();
+connectToDatabase()
 
 // addPlayer POST route, for future use
 app.post('/addPlayer', async (req, res) => {
@@ -34,20 +36,42 @@ app.post('/addPlayer', async (req, res) => {
   }
 })
 
-//make GET request to browse endpoint, extract only 3 for now
-app.get('/asteroids', async (req, res) => {
-  try {
-    const response = await axios.get(
-      `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`
-    )
-    const asteroids = response.data.near_earth_objects.slice(0, 3)
-    res.json(asteroids) // now localhost:3000/asteroids will display their JSON
-  } catch (error) {
-    console.log(error)
-    res
-      .status(500)
-      .send('Error occurred while fetching data from NASA API', error)
+app.get('/asteroids', (req, res) => {
+  // Check if cache file exists
+  if (fs.existsSync(CACHE_FILE)) {
+    const cachedDataRaw = fs.readFileSync(CACHE_FILE, 'utf-8')
+    const cachedData = JSON.parse(cachedDataRaw)
+
+    const currentTime = new Date().getTime()
+
+    // If cache is still fresh, return the cached data
+    if (currentTime - cachedData.timestamp <= CACHE_DURATION) {
+      return res.json(cachedData.asteroids)
+    }
   }
+
+  // If cache is not available or stale, fetch from API
+  axios
+    .get(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${API_KEY}`)
+    .then((response) => {
+      const asteroids = response.data.near_earth_objects.slice(0, 3)
+
+      // Write to cache file
+      const dataToCache = {
+        timestamp: new Date().getTime(),
+        asteroids: asteroids,
+      }
+
+      fs.writeFileSync(CACHE_FILE, JSON.stringify(dataToCache))
+
+      res.json(asteroids)
+    })
+    .catch((error) => {
+      console.log(error)
+      res
+        .status(500)
+        .send('Error occurred while fetching data from NASA API', error)
+    })
 })
 
 // Catch-all route, will direct any invalid routes back to landing page
