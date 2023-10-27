5ae367aab712f6f3599b6cccba7608ca8a2f1603
aflores4 <canonball575@gmail.com>
2023-10-27 16:49:10 -0600

    Further implemented more art
    Implemented more animations, implemented the health and shield bars, also implemented shooting animations. Main changes were all in animation class, had to edit player very slightly just to add more identifiers to player object.

--- frontend/src/game/animation.js
+++ frontend/src/game/animation.js
@@ -1,4 +1,5 @@
 export function loadPlayerAnimations(scene){
+  // NORMAL ANIMS
   scene.load.spritesheet("walk", "./assets/sprites/player_walk.png",{
       frameWidth: 32,
       frameHeight: 64}
@@ -22,13 +23,102 @@ export function loadPlayerAnimations(scene){
   scene.load.spritesheet("boost", "./assets/sprites/player_jump_boost.png",{
     frameWidth: 32,
     frameHeight: 64}
-);
+  );
+
+
+
+  //SHOOTING ANIMS
+
+  scene.load.spritesheet("walk_shoot", "./assets/sprites/player_walk_shoot.png",{
+    frameWidth: 32,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("run_shoot", "./assets/sprites/player_run_shoot.png",{
+    frameWidth: 32,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("jump_shoot", "./assets/sprites/player_jump_shoot.png",{
+    frameWidth: 32,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("idle_shoot", "./assets/sprites/player_idle_shoot.png",{
+    frameWidth: 32,
+    frameHeight: 64}
+  );
+
+
+
+
+  // SHOOTING ARMS ANIMS
+
+  scene.load.spritesheet("unholster", "./assets/sprites/shooting/player_unholster.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("holster", "./assets/sprites/shooting/player_holster.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+  
+  scene.load.spritesheet("shoot_holstered", "./assets/sprites/shooting/player_shoot_holstered.png",{
+    frameWidth: 32,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("shoot_straight_down", "./assets/sprites/shooting/player_shoot_straight_down.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("shoot_down", "./assets/sprites/shooting/player_shoot_down.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("shoot_straight", "./assets/sprites/shooting/player_shoot_straight.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("shoot_up", "./assets/sprites/shooting/player_shoot_up.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+  scene.load.spritesheet("shoot_straight_up", "./assets/sprites/shooting/player_shoot_straight_up.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+
+
+  // GUN ANIMS
+
+  scene.load.spritesheet("shoot_straight_up", "./assets/sprites/shooting/player_shoot_straight_up.png",{
+    frameWidth: 48,
+    frameHeight: 64}
+  );
+
+
+  scene.load.spritesheet("strap", "./assets/sprites/weapons/backpack_strap.png",{
+    frameWidth: 64,
+    frameHeight: 64}
+  );
+
+  scene.load.image('pistol', './assets/sprites/weapons/pistol.png');
+  scene.load.image('pistol_holstered', './assets/sprites/weapons/pistol_holstered.png');
+  scene.load.image('ar', './assets/sprites/weapons/ar.png');
+  scene.load.image('ar_holstered', './assets/sprites/weapons/ar_holstered.png');
 }
 
 
-
 export function createPlayerAnimations(scene){
-  // Create the animations
+  // CREATES ANIMATIONS
+
   scene.anims.create({
     key: 'player_walk',
     frames: scene.anims.generateFrameNumbers('walk'),
@@ -50,7 +140,26 @@ export function createPlayerAnimations(scene){
     repeat: -1,
   })
 
+  scene.anims.create({
+    key: 'player_walk_shoot',
+    frames: scene.anims.generateFrameNumbers('walk_shoot'),
+    frameRate: 10,
+    repeat: -1,
+  })
 
+  scene.anims.create({
+    key: 'player_idle_shoot',
+    frames: scene.anims.generateFrameNumbers('idle_shoot'),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  scene.anims.create({
+    key: 'player_jump_shoot',
+    frames: scene.anims.generateFrameNumbers('jump_shoot'),
+    frameRate: 16,
+    repeat: -1,
+  })
 
   scene.anims.create({
     key: 'player_jump_boost',
@@ -59,10 +168,67 @@ export function createPlayerAnimations(scene){
     repeat: -1,
   })
 
+  scene.anims.create({
+    key: 'player_holster',
+    frames: scene.anims.generateFrameNumbers('holster'),
+    frameRate: 10,
+    repeat: 0,
+  })
+
+  scene.anims.create({
+    key: 'player_unholster',
+    frames: scene.anims.generateFrameNumbers('unholster'),
+    frameRate: 100,
+    repeat: 0,
+  })
+
+  scene.anims.create({
+    key: 'player_shoot_holstered',
+    frames: scene.anims.generateFrameNumbers('shoot_holstered'),
+    frameRate: 10,
+    repeat: -1,
+  })
+  
+  scene.anims.create({
+    key: 'player_shoot_straight_down',
+    frames: scene.anims.generateFrameNumbers('shoot_straight_down'),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  scene.anims.create({
+    key: 'player_shoot_down',
+    frames: scene.anims.generateFrameNumbers('shoot_down'),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  scene.anims.create({
+    key: 'player_shoot_straight',
+    frames: scene.anims.generateFrameNumbers('shoot_straight'),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  scene.anims.create({
+    key: 'player_shoot_up',
+    frames: scene.anims.generateFrameNumbers('shoot_up'),
+    frameRate: 10,
+    repeat: -1,
+  })
+
+  scene.anims.create({
+    key: 'player_shoot_straight_up',
+    frames: scene.anims.generateFrameNumbers('shoot_straight_up'),
+    frameRate: 10,
+    repeat: -1,
+  })
 
 
 
-  // sets the animator objects for player
+
+  // CREATES ANIMATOR OBJECTS
+
   scene.player.boostAnimator = scene.playerAnimation = scene.add.sprite(
     scene.player.sprite.x,
     scene.player.sprite.y,
@@ -75,9 +241,35 @@ export function createPlayerAnimations(scene){
     'idle'
   )
 
+  scene.player.shootingAnimator = scene.playerAnimation = scene.add.sprite(
+    scene.player.sprite.x,
+    scene.player.sprite.y,
+    'idle_shoot'
+  )
+
+  scene.player.armAnimator = scene.playerAnimation = scene.add.sprite(
+    scene.player.sprite.x,
+    scene.player.sprite.y,
+    'shoot_straight'
+  )
+
+
+
+  scene.player.weaponSprite = scene.add.sprite(scene.player.x, scene.player.y, 'pistol');
+  scene.player.weaponHolsteredSprite = scene.add.sprite(scene.player.x, scene.player.y, 'pistol_holstered');
+  scene.player.weaponSprite.alpha = 0;
+
+  scene.player.strapAnimator = scene.playerAnimation = scene.add.sprite(
+    scene.player.sprite.x,
+    scene.player.sprite.y,
+    'strap'
+  )
   
   scene.player.boostAnimator.alpha = 0;
 }
+
+var holsterCounter;
+
 
 export function updatePlayerAnimations(scene){
   scene.player.animator.x = scene.player.sprite.x
@@ -87,15 +279,99 @@ export function updatePlayerAnimations(scene){
   scene.player.boostAnimator.y = scene.player.sprite.y
   scene.player.boostAnimator.alpha = 0;
 
+  scene.player.strapAnimator.x = scene.player.sprite.x
+  scene.player.strapAnimator.y = scene.player.sprite.y
+
+  scene.player.shootingAnimator.x = scene.player.sprite.x;
+  scene.player.shootingAnimator.y = scene.player.sprite.y;
+
+  scene.player.armAnimator.x = scene.player.sprite.x;
+  scene.player.armAnimator.y = scene.player.sprite.y;
+
+  scene.player.weaponSprite.x = scene.player.sprite.x;
+  scene.player.weaponSprite.y = scene.player.sprite.y;
+
+  scene.player.weaponHolsteredSprite.x = scene.player.sprite.x;
+  scene.player.weaponHolsteredSprite.y = scene.player.sprite.y;
+
+  
+
+  // Sets alpha depending on shooting status
+  if (!scene.player.holstered){ //shooting
+    scene.player.animator.alpha = 0;
+    scene.player.shootingAnimator.alpha = 100;
+    scene.player.armAnimator.alpha = 100;
+    //scene.player.weaponSprite.alpha = 100;
+    scene.player.weaponHolsteredSprite.alpha = 0;
+  }
+  else{ // not shooting
+    scene.player.animator.alpha = 100;
+    scene.player.shootingAnimator.alpha = 0;
+    scene.player.armAnimator.alpha = 0;
+    scene.player.weaponSprite.alpha = 0;
+    scene.player.weaponHolsteredSprite.alpha = 100;
+  }
+
+  if (scene.player.unholstering){
+    if (!scene.player.armAnimator.anims.isPlaying){
+      scene.player.unholstering = false;
+      scene.player.armAnimator.play('player_shoot_straight', true);
+      scene.player.holstered = false;
+    }
+  }
+
+  if (scene.player.holstering){
+    if (!scene.player.armAnimator.anims.isPlaying){
+      scene.player.holstered = true;
+      scene.player.holstering = false;
+      scene.player.armAnimator.alpha = 0;
+    }
+  }
+
+  if (scene.player.shoot){
+    if (scene.player.holstered){ // play unholster
+      scene.player.armAnimator.play('player_unholster', true);
+      scene.player.unholstering = true;
+      scene.player.holstered = false;
+    }
+
+    holsterCounter = 250;
+  }
+  else{
+    holsterCounter -= 1;
+
+    if (holsterCounter <= 0){
+      // player holster anim
+      scene.player.armAnimator.play('player_holster', true);
+      scene.player.holstering = true;
+    }
+    //count
+  }
 
 
   if (scene.player.facing == 'left'){ // look backward
     scene.player.animator.setFlipX(true) 
+    scene.player.shootingAnimator.setFlipX(true) 
     scene.player.boostAnimator.setFlipX(true) 
+    scene.player.strapAnimator.setFlipX(true)
+
+    scene.player.armAnimator.setFlipX(true)
+    scene.player.armAnimator.x = scene.player.sprite.x - 8;
+
+    scene.player.weaponSprite.setFlipX(true);
+    scene.player.weaponHolsteredSprite.setFlipX(true);
   }
   else { // look forward
     scene.player.animator.setFlipX(false) 
+    scene.player.shootingAnimator.setFlipX(false) 
     scene.player.boostAnimator.setFlipX(false) 
+    scene.player.strapAnimator.setFlipX(false)
+    
+    scene.player.armAnimator.setFlipX(false)
+    scene.player.armAnimator.x = scene.player.sprite.x + 8;
+
+    scene.player.weaponSprite.setFlipX(false);
+    scene.player.weaponHolsteredSprite.setFlipX(false);
   }
 
 
@@ -106,14 +382,17 @@ export function updatePlayerAnimations(scene){
     }
     else{
       scene.player.animator.anims.play('player_jump', true)
+      scene.player.shootingAnimator.anims.play('player_jump_shoot', true)
     }
 
   }
   else if (scene.player.walking){
     scene.player.animator.anims.play('player_walk', true)
+    scene.player.shootingAnimator.anims.play('player_walk_shoot', true)
   }
   else{
     scene.player.animator.anims.play('player_idle', true)
+    scene.player.shootingAnimator.anims.play('player_idle_shoot', true)
   }
 }
 
@@ -177,3 +456,5 @@ export function updateEnemyAnimations(scene, enemy){
   enemy.animator.x = enemy.sprite.x; // updates animation position
   enemy.animator.y = enemy.sprite.y; // updates animation position
 }
+
+
